5e639b9f4330b544de3e45fec63ad7870a6b25c9
Tadzio <tadzio.dluoglecki@gmail.com>
2019-06-11 16:49:50 -0700

    fucked v2

--- wp-content/themes/elston/searchform.php
+++
@@ -1,8 +0,0 @@
-<form method="get" id="searchform" action="<?php echo esc_url(home_url('/')); ?>" class="searchform" >
-	<div class="input-group">
-		<input type="text" name="s" id="s" placeholder="<?php esc_html_e('Search...', 'elston'); ?>" />
-		<span class="input-group-btn">
-			<input type="submit" id="searchsubmit" value="<?php esc_html_e('Search', 'elston'); ?>" />
-		</span>
-	</div><!-- /input-group -->
-</form><?php
